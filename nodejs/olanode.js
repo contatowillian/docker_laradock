98b676dae4b92665900046f2d27e218e35988c69
contatowillian <contatowillian@gmail.com>
2019-06-26 21:05:47 -0300

    Docker laravel

---
+++ nodejs/olanode.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+var https = require('https')
+var querystring = require('querystring')
+
+var arguments = process.argv.splice(2, process.argv.length -1).join(' ')
+var search    = querystring.stringify({ address: arguments })
+
+https
+  .get('https://maps.googleapis.com/maps/api/geocode/json?' + search, function(res){
+    var data = ''
+
+    res.on('data', function(newData){
+      data += newData
+    });
+
+    res.on('end', function(){
+      var location = JSON.parse(data).results[0].geometry.location
+    })
+  })
